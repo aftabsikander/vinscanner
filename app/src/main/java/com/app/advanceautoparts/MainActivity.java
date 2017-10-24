772a8ead7bcaf9848c492e2bd7cebefc39e0b3ef
Aftab Ali <aftab.a.sikander@gmail.com>
2017-10-25 04:32:38 +0500

    Fixed butter knife crash issue and included heap size attribute
    Signed-off-by: Aftab Ali <aftab.a.sikander@gmail.com>

--- app/src/main/java/com/app/advanceautoparts/MainActivity.java
+++ app/src/main/java/com/app/advanceautoparts/MainActivity.java
@@ -3,10 +3,12 @@ package com.app.advanceautoparts;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.AppCompatTextView;
+import android.widget.Toast;
 
 import com.pixotech.android.scanner.library.ScannerActivity;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends ScannerActivity {
 
@@ -18,12 +20,14 @@ public class MainActivity extends ScannerActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addLayout(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
     @Override
     public void handleDecode(String s) {
         vibrate();
         vinTextView.setText(s);
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
         startScan();
     }
 }
